3fb07fb53bb92c86bc22934813ce046ff875e6dc
nombre <81451283+nombre15@users.noreply.github.com>
2022-06-24 00:17:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,31 +9,40 @@ int main()
 {
 
     srand(time(0));
-    string adjectives_list, animals_list;
+    string adjectives_list, animals_list, personalities_list;
     int adjectives_lines = 0;
     int animals_lines = 0;
+    int personalities_lines = 0;
     ifstream File("adjectives.txt");
     ifstream File2("animals.txt");
+    ifstream File3("personalities.txt");
 
     while(getline(File, adjectives_list)){
 
             adjectives_lines++;
     }
 
-     while(getline(File2, animals_list)){
+    while(getline(File2, animals_list)){
 
             animals_lines++;
     }
 
-    cout << "Press enter to generate a new fursona" << endl << endl;
+    while(getline(File3, personalities_list)){
+
+            personalities_lines++;
+    }
+
+    cout << "Press enter to generate a new fursona" << endl;
+    cout << "Press space to give a personality to your fursona" << endl << endl;
 
     do{
         int random_adjective = rand() %adjectives_lines;
         int random_animal = rand() % animals_lines;
-        int lines = 0;
-        int lines2 = 0;
+        int random_personality = rand() % personalities_lines;
+        int lines = 0, lines2 = 0, lines3 = 0;
         ifstream File("adjectives.txt");
         ifstream File2("animals.txt");
+        ifstream File3("personalities.txt");
 
         while(getline(File, adjectives_list)){
 
@@ -49,13 +58,34 @@ int main()
 
             if(lines2 == random_animal){
 
-                cout << " " << animals_list << endl << endl;
+                cout << " " << animals_list;
             }
 
             lines2++;
         }
 
+       //this makes it work funny and now you have to press enter twice to generate a new regular fursona, idk why still have to fix it
+        int g = getch();
+        if(g == 32){
+
+            while(getline(File3, personalities_list)){
+
+                if(lines3 == random_personality){
+
+                    cout << " who " << personalities_list << endl << endl;
+                }
+
+                lines3++;
+            }
+        }
+
+        else{
+            cout << endl << endl;
+        }
+
+
         int c = getch();
         if (c == 0 || c == 224) getch();
     }while(1==1);
 }
+
